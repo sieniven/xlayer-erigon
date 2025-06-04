80c26d7cbb092d9e5330ab78e21cb35c0318dbfb
zhe.yang <pandayt@163.com>
2025-06-04 17:31:26 +0800

    run test case in test directory

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+set -e
+
+DOCKER_TEST_IMAGE="xlayer-erigon-test"
+DOCKER_PARAM=("tests" "lint" "check_chinese_characters" "test-unwind-default" "test-unwind-split-db")
+LOCAL_PARAM=("test-e2e" "test-data-loss" "test-executor" "kurtosis-cdk" "resequence-default" "resequence-ac-split")
+
 function create_dockerignore() {
     ignorefile=$1
     echo "**/*.a" > $ignorefile
@@ -14,14 +20,74 @@ function create_dockerignore() {
     echo "mainnet" >> $ignorefile
 }
 
-# change to build directory to avoid using .dockerignore in the root directory
-mkdir -p build && cd build
+function is_docker_param() {
+    for param in "${DOCKER_PARAM[@]}"; do
+        if [ "$1" == "$param" ]; then
+            return 0
+        fi
+    done
+    return 1
+}
 
-create_dockerignore ".dockerignore"
+function is_local_param() {
+    for param in "${LOCAL_PARAM[@]}"; do
+        if [ "$1" == "$param" ]; then
+            return 0
+        fi
+    done
+    return 1
+}
 
-docker build -t xlayer-erigon-test -f ../Dockerfile.test ..
-docker run --rm -it xlayer-erigon-test
+function cleanup() {
+    if [ -f .dockerignore.bak ]; then
+        mv .dockerignore.bak .dockerignore
+    fi
+}
 
-cd ..
+function build_docker_image() {
+    mv .dockerignore .dockerignore.bak
+    trap cleanup EXIT      # normal exit
+    trap cleanup SIGINT    # Ctrl+C
+    trap cleanup SIGTERM   # kill 
+    trap cleanup SIGQUIT   # Ctrl+\
+    trap cleanup SIGHUP    # terminal shutdown
+
+    create_dockerignore ".dockerignore"
+
+    docker build -t $DOCKER_TEST_IMAGE -f ./Dockerfile.test .
+    cleanup # clean up in advance
+}
+
+function run_in_docker() {
+    docker run --rm $DOCKER_TEST_IMAGE $1
+}
+
+function test_all() {
+    build_docker_image
+
+    for param in "${DOCKER_PARAM[@]}"; do
+        run_in_docker $param
+    done
+    for param in "${LOCAL_PARAM[@]}"; do
+        cd ./test
+        make $param
+    done
+}
+
+
+cd $(dirname $0)
+
+if [ "$1" == "all" ]; then
+    test_all
+elif is_local_param $1; then
+    cd ./test
+    make $1
+elif is_docker_param $1; then
+    build_docker_image
+    run_in_docker $1
+else
+    echo "Error: invalid parameter '$1'"
+    exit 1
+fi
 
 
