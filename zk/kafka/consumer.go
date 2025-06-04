47e08dc4b1cec16c467bd1cc0346e26867cb6306
Niven <sieniven@gmail.com>
2025-06-04 15:23:26 +0800

    Fix consumer

--- zk/kafka/consumer.go
+++ zk/kafka/consumer.go
@@ -8,10 +8,11 @@ import (
 	"github.com/IBM/sarama"
 	"github.com/ledgerwatch/erigon/eth/ethconfig"
 	kafkaTypes "github.com/ledgerwatch/erigon/zk/kafka/types"
+	"github.com/ledgerwatch/log/v3"
 )
 
 type KafkaConsumer struct {
-	consumer sarama.Consumer
+	consumer sarama.ConsumerGroup
 	config   ethconfig.KafkaConfig
 }
 
@@ -19,46 +20,72 @@ func NewKafkaConsumer(config ethconfig.KafkaConfig) (*KafkaConsumer, error) {
 	saramaConfig := sarama.NewConfig()
 	saramaConfig.Version = DEFAULT_VERSION
 	saramaConfig.ClientID = config.ClientID
+	saramaConfig.Consumer.Offsets.Initial = sarama.OffsetOldest
+	saramaConfig.Consumer.Offsets.AutoCommit.Enable = true
 
-	// Create sync producer
-	consumer, err := sarama.NewConsumer(config.BootstrapServers, saramaConfig)
+	// Create consumer group
+	consumerGroup, err := sarama.NewConsumerGroup(config.BootstrapServers, config.ClientID, saramaConfig)
 	if err != nil {
-		return nil, fmt.Errorf("error creating Kafka producer: %v", err)
+		return nil, fmt.Errorf("error creating Kafka consumer: %v", err)
 	}
 
 	return &KafkaConsumer{
-		consumer: consumer,
+		consumer: consumerGroup,
 		config:   config,
 	}, nil
 }
 
-// ConsumeKafkaTransactions starts consuming transaction messages from the specified topic
-func (client *KafkaConsumer) ConsumeKafkaTransactions(ctx context.Context, txMsgsChan chan kafkaTypes.TransactionMessage, errorChan chan error) {
-	// Create a partition consumer for the topic
-	partitionConsumer, err := client.consumer.ConsumePartition(client.config.Topic, 0, sarama.OffsetNewest)
-	if err != nil {
-		errorChan <- fmt.Errorf("failed to create partition consumer: %v", err)
-		return
-	}
-	defer partitionConsumer.Close()
+type consumerGroupHandler struct {
+	ctx        context.Context
+	txMsgsChan chan kafkaTypes.TransactionMessage
+	errorChan  chan error
+	logger     log.Logger
+}
 
-	// Start consuming messages
+func (h *consumerGroupHandler) Setup(session sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (h *consumerGroupHandler) Cleanup(_ sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (h *consumerGroupHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	h.logger.Info("Starting kafka consumption for topic %s partition %d offset %d", claim.Topic(), claim.Partition(), claim.InitialOffset())
+	fmt.Printf("XXX Starting kafka consumption for topic %s partition %d offset %d\n", claim.Topic(), claim.Partition(), claim.InitialOffset())
 	for {
 		select {
-		case <-ctx.Done():
-			errorChan <- fmt.Errorf("context done - stopping kafka consumer")
-			return
-		case msg := <-partitionConsumer.Messages():
+		case <-h.ctx.Done():
+			return fmt.Errorf("context done - stopping consume claim")
+		case msg, ok := <-claim.Messages():
+			if !ok {
+				return nil
+			}
 			var txMsg kafkaTypes.TransactionMessage
 			if err := json.Unmarshal(msg.Value, &txMsg); err != nil {
-				errorChan <- fmt.Errorf("ConsumeKafkaTransactions error: error unmarshaling transaction message, %v", err)
-				return
+				h.errorChan <- fmt.Errorf("consume claim error, unmarshaling transaction message: %v", err)
+				return err
 			}
-			txMsgsChan <- txMsg
-		case err := <-partitionConsumer.Errors():
-			errorChan <- fmt.Errorf("ConsumeKafkaTransactions error: %v", err)
-			return
+			h.txMsgsChan <- txMsg
+			session.MarkMessage(msg, "")
 		}
+	}
+}
+
+// ConsumeKafkaTransactions starts consuming transaction messages from the specified topic
+func (client *KafkaConsumer) ConsumeKafkaTransactions(ctx context.Context, txMsgsChan chan kafkaTypes.TransactionMessage, errorChan chan error, logger log.Logger) {
+	handler := &consumerGroupHandler{
+		ctx:        ctx,
+		txMsgsChan: txMsgsChan,
+		errorChan:  errorChan,
+		logger:     logger,
+	}
+
+	topics := []string{client.config.Topic}
+	err := client.consumer.Consume(ctx, topics, handler)
+	if err != nil {
+		errorChan <- fmt.Errorf("ConsumeKafkaTransactions error: %v", err)
+		return
 	}
 }
 
--- zk/kafka/kafka_test.go
+++ zk/kafka/kafka_test.go
@@ -9,38 +9,16 @@ import (
 	"github.com/ledgerwatch/erigon/common/u256"
 	types1 "github.com/ledgerwatch/erigon/core/types"
 	"github.com/ledgerwatch/erigon/eth/ethconfig"
+	kafkaTypes "github.com/ledgerwatch/erigon/zk/kafka/types"
+	"github.com/ledgerwatch/log/v3"
 	"gotest.tools/v3/assert"
 )
 
 var (
-	testFromAddr = libcommon.HexToAddress("095e7baea6a6c7c4c2dfeb977efac326af552d87")
-	testToAddr   = libcommon.HexToAddress("b94f5374fce5edbc8e2a8697c15331677e6ebf0b")
-	addr         = libcommon.HexToAddress("0x0000000000000000000000000000000000000001")
-)
-
-func TestKafkaConsumer(t *testing.T) {
-	cfg := ethconfig.KafkaConfig{
-		Enable:           true,
-		BootstrapServers: []string{"0.0.0.0:9094"},
-		Topic:            "xlayer-test",
-		ClientID:         "xlayer-test-consumer",
-	}
-	_, err := NewKafkaConsumer(cfg)
-	assert.NilError(t, err)
-}
-
-func TestKafkaProducer(t *testing.T) {
-	cfg := ethconfig.KafkaConfig{
-		Enable:           true,
-		BootstrapServers: []string{"0.0.0.0:9094"},
-		Topic:            "xlayer-test",
-		ClientID:         "xlayer-test-producer",
-	}
-	producer, err := NewKafkaProducer(cfg)
-	assert.NilError(t, err)
-
-	sigBytes := "98ff921201554726367d2be8c804a7ff89ccf285ebc57dff8ae4c44b9c19ac4a8887321be575c8095f789dd4c743dfe42c1820f9231f98a962b210e3ac2452a301"
-	rightvrsTx, _ := types1.NewTransaction(
+	testFromAddr  = libcommon.HexToAddress("095e7baea6a6c7c4c2dfeb977efac326af552d87")
+	testToAddr    = libcommon.HexToAddress("b94f5374fce5edbc8e2a8697c15331677e6ebf0b")
+	sigBytes      = "98ff921201554726367d2be8c804a7ff89ccf285ebc57dff8ae4c44b9c19ac4a8887321be575c8095f789dd4c743dfe42c1820f9231f98a962b210e3ac2452a301"
+	rightvrsTx, _ = types1.NewTransaction(
 		3,
 		testToAddr,
 		uint256.NewInt(10),
@@ -51,9 +29,8 @@ func TestKafkaProducer(t *testing.T) {
 		*types1.LatestSignerForChainID(nil),
 		libcommon.Hex2Bytes(sigBytes),
 	)
-	rightvrsTx.SetSender(testFromAddr)
 
-	rightvrsTxReceipt := &types1.Receipt{
+	rightvrsTxReceipt = &types1.Receipt{
 		PostState:         libcommon.Hash{2}.Bytes(),
 		CumulativeGasUsed: 3,
 		Logs: []*types1.Log{
@@ -64,6 +41,62 @@ func TestKafkaProducer(t *testing.T) {
 		ContractAddress: libcommon.BytesToAddress([]byte{0x02, 0x22, 0x22}),
 		GasUsed:         2,
 	}
+)
+
+func TestKafkaConsumer(t *testing.T) {
+	rightvrsTx.SetSender(testFromAddr)
+	cfg := ethconfig.KafkaConfig{
+		Enable:           true,
+		BootstrapServers: []string{"0.0.0.0:9094"},
+		Topic:            "xlayer-test",
+		ClientID:         "xlayer-test-consumer",
+	}
+	consumer, err := NewKafkaConsumer(cfg)
+	assert.NilError(t, err)
+	ctx, ctxWithCancel := context.WithCancel(context.Background())
+	txMsgsChan := make(chan kafkaTypes.TransactionMessage, 10)
+	errorChan := make(chan error, 10)
+	go consumer.ConsumeKafkaTransactions(ctx, txMsgsChan, errorChan, log.New())
+
+	// Verify messages
+	for i := 0; i < 10; i++ {
+		select {
+		case err := <-errorChan:
+			t.Fatalf("Received error from consumer: %v", err)
+		case txMsg := <-txMsgsChan:
+			assert.Equal(t, txMsg.BlockNumber, i)
+			assert.Equal(t, int(txMsg.Type), types1.LegacyTxType)
+			assert.Equal(t, txMsg.Hash, rightvrsTx.Hash().String())
+			assert.Equal(t, txMsg.From, testFromAddr.String())
+			assert.Equal(t, txMsg.ChainID, rightvrsTx.GetChainID().Uint64())
+			assert.Equal(t, txMsg.Nonce, rightvrsTx.GetNonce())
+			assert.Equal(t, txMsg.Gas, uint64(2000))
+			assert.Equal(t, txMsg.To, testToAddr.String())
+			assert.Equal(t, txMsg.Value, "10")
+			assert.Equal(t, txMsg.Data, "5544")
+			v, r, s := rightvrsTx.RawSignatureValues()
+			assert.Equal(t, txMsg.R, r.Hex())
+			assert.Equal(t, txMsg.S, s.Hex())
+			assert.Equal(t, txMsg.V, v.Hex())
+			assert.Equal(t, txMsg.GasPrice, "1")
+			assertReceipt(t, txMsg, rightvrsTxReceipt)
+		}
+	}
+	ctxWithCancel()
+	err = consumer.Close()
+	assert.NilError(t, err)
+}
+
+func TestKafkaProducer(t *testing.T) {
+	rightvrsTx.SetSender(testFromAddr)
+	cfg := ethconfig.KafkaConfig{
+		Enable:           true,
+		BootstrapServers: []string{"0.0.0.0:9094"},
+		Topic:            "xlayer-test",
+		ClientID:         "xlayer-test-producer",
+	}
+	producer, err := NewKafkaProducer(cfg)
+	assert.NilError(t, err)
 
 	for i := 0; i < 10; i++ {
 		err = producer.SendKafkaTransaction(context.Background(), uint64(i), rightvrsTx, rightvrsTxReceipt)
@@ -72,4 +105,28 @@ func TestKafkaProducer(t *testing.T) {
 
 	err = producer.Close()
 	assert.NilError(t, err)
+}
+
+func assertReceipt(t *testing.T, msg kafkaTypes.TransactionMessage, receipt *types1.Receipt) {
+	assert.Equal(t, msg.Receipt.Type, receipt.Type)
+	assert.Equal(t, string(msg.Receipt.PostState), string(receipt.PostState))
+	assert.Equal(t, msg.Receipt.Status, receipt.Status)
+	assert.Equal(t, msg.Receipt.CumulativeGasUsed, receipt.CumulativeGasUsed)
+	assert.Equal(t, msg.Receipt.Bloom, receipt.Bloom)
+	assert.Equal(t, len(msg.Receipt.Logs), len(receipt.Logs))
+	for i := range msg.Receipt.Logs {
+		assert.Equal(t, msg.Receipt.Logs[i].Address.String(), receipt.Logs[i].Address.String())
+		assert.Equal(t, len(msg.Receipt.Logs[i].Topics), len(receipt.Logs[i].Topics))
+		for j := range msg.Receipt.Logs[i].Topics {
+			assert.Equal(t, msg.Receipt.Logs[i].Topics[j].String(), receipt.Logs[i].Topics[j].String())
+		}
+		assert.Equal(t, string(msg.Receipt.Logs[i].Data), string(receipt.Logs[i].Data))
+	}
+
+	assert.Equal(t, msg.Receipt.TxHash, receipt.TxHash)
+	assert.Equal(t, msg.Receipt.ContractAddress.String(), receipt.ContractAddress.String())
+	assert.Equal(t, msg.Receipt.GasUsed, receipt.GasUsed)
+	assert.Equal(t, msg.Receipt.BlockHash, receipt.BlockHash)
+	assert.Equal(t, msg.Receipt.BlockNumber, receipt.BlockNumber)
+	assert.Equal(t, msg.Receipt.TransactionIndex, receipt.TransactionIndex)
 }
