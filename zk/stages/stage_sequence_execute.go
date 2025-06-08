0181ef9d2e4d8c4af09c6e20d9d3e3224ef11b3a
KyrinCode <kyrincode@qq.com>
2025-06-08 19:42:33 +0800

    Generate transactional changeset

--- zk/stages/stage_sequence_execute.go
+++ zk/stages/stage_sequence_execute.go
@@ -612,7 +612,7 @@ BatchLoop:
 
 				effectiveGas := batchState.blockState.getL1EffectiveGases(cfg, i)
 
-				receipt, execResult, innerTxs, anyOverflow, err := attemptAddTransaction(cfg, sdb, ibs, &blockContext, header, transaction, effectiveGas, batchState.isL1Recovery(), batchState.forkId, l1TreeUpdateIndex, ethBlockGasPool)
+				receipt, execResult, innerTxs, changeset, anyOverflow, err := attemptAddTransaction(cfg, sdb, ibs, &blockContext, header, transaction, effectiveGas, batchState.isL1Recovery(), batchState.forkId, l1TreeUpdateIndex, ethBlockGasPool)
 				if err != nil {
 					metrics.GetLogStatistics().CumulativeCounting(metrics.ProcessingInvalidTxCounter)
 					if batchState.isLimboRecovery() {
@@ -683,6 +683,8 @@ BatchLoop:
 					if cfg.zk.XLayer.Kafka.Enable {
 						cfg.txKafkaProducer.SendKafkaTransaction(ctx, blockNumber, transaction, receipt, innerTxs)
 					}
+
+					log.Info(fmt.Sprintf("[%s] changeset", logPrefix), "txhash", txHash, "changeset", changeset)
 				}
 
 				// We will only update the processed index in resequence job if there isn't overflow
