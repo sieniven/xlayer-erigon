1f4f2b7abd1d30e67f373ca8f30c33bf562fed13
jianguo <zjg555543@163.com>
2025-06-09 12:41:32 +0800

    update

--- test-fep-pp/5-build-ckd-node.sh
+++ test-fep-pp/5-build-ckd-node.sh
@@ -2,6 +2,26 @@
 set -e
 # set -x
 
+#!/bin/bash
+PWD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+input=${1:-0}
+
+# checking input
+if ! [[ "$input" =~ ^[0-7]$ ]]; then
+    echo "Error" >&2
+    exit 1
+fi
+
+sed_inplace() {
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' "$@"
+  else
+    sed -i "$@"
+  fi
+}
+
+
 if [ ! -d "./xlayer-cdk" ]; then
   echo "Cloning contract repository..."
   git clone -b upstream/v0.5.4-rc1 https://github.com/okx/xlayer-cdk.git
@@ -10,3 +30,18 @@ fi
 cd ./xlayer-cdk
 echo "Cleaning and resting contract repository..."
 rm -rf *; git reset --hard; git checkout upstream/v0.5.4-rc1
+
+make build-docker
+
+
+cd $PWD_DIR
+
+
+DOCK_CONFIG_FILE="./docker-compose.yml"
+sed_inplace "s|image: zjg555543/cdk:v0.5.4-rc1|image: cdk|g" "$DOCK_CONFIG_FILE"
+
+CDK_CONFIG_FILE="config/cdk-node-config.toml"
+sed_inplace "s|\(LossCertType[[:space:]]*=[[:space:]]*\)[0-7]|\1$input|g" "$CDK_CONFIG_FILE"
+
+docker-compose stop xlayer-cdk-node
+docker-compose up xlayer-cdk-node
