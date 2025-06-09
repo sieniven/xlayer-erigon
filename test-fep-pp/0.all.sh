6ae3b90a48ffafe3b93b46a788a7acff429c9f59
jianguo <zjg555543@163.com>
2025-06-09 14:13:19 +0800

    update

--- test-fep-pp/0.all.sh
+++ test-fep-pp/0.all.sh
@@ -3,7 +3,8 @@ chmod +x 1-init-fep.sh
 chmod +x 2-update-rollupmgr.sh
 chmod +x 3-update-pp.sh
 chmod +x 4-restart-service.sh
-chmod +x 5-test-sig.sh
+chmod +x 5-build-ckd-node.sh
+chmod +x 6-test-sig.sh
 
 ./1-init-fep.sh
 ./2-update-rollupmgr.sh
