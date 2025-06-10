12c8c6c87676c75d93be2f99b03cb5f1b5c95fbf
jianguo <zjg555543@163.com>
2025-06-10 10:28:05 +0800

    update

--- test-fep-pp/4-restart-service.sh
+++ test-fep-pp/4-restart-service.sh
@@ -13,6 +13,10 @@ sed_inplace() {
 DOCK_CONFIG_FILE="./docker-compose.yml"
 sed_inplace "s|image: cdk-erigon|zjg555543/xlayer-erigon:dev-pp-6dd537c|g" "$DOCK_CONFIG_FILE"
 
+DOCK_CONFIG_FILE="./config/test.erigon.seq.config.yaml"
+sed_inplace "s|zkevm.executor-strict: false| |g" "$DOCK_CONFIG_FILE"
+sed_inplace "s|zkevm.witness-full: false| |g" "$DOCK_CONFIG_FILE"
+sed_inplace "s|zkevm.executor-mock: true| |g" "$DOCK_CONFIG_FILE"
 
 
 make run-new
