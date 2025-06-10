8fd932a7846413a56473d8610fe6e480b80fee95
jianguo <zjg555543@163.com>
2025-06-10 10:21:02 +0800

    update

--- test-fep-pp/1-init-fep.sh
+++ test-fep-pp/1-init-fep.sh
@@ -34,6 +34,10 @@ echo "Starting xlayer-mock-l1-network..."
 docker-compose up -d xlayer-mock-l1-network
 sleep 5
 
+git checkout docker-compose.yml
+git checkout test.erigon.seq.config.yaml
+git checkout test.erigon.rpc.config.yaml
+
 echo "Sending funds to deployer..."
 cast send -f $RICH_ADDRESS --private-key $RICH_PRIVATE_KEY --value 3ether --legacy $DEPLOYER_ADDRESS
 
--- test-fep-pp/4-restart-service.sh
+++ test-fep-pp/4-restart-service.sh
@@ -2,4 +2,17 @@
 set -e
 # set -x
 
+sed_inplace() {
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' "$@"
+  else
+    sed -i "$@"
+  fi
+}
+
+DOCK_CONFIG_FILE="./docker-compose.yml"
+sed_inplace "s|image: cdk-erigon|zjg555543/xlayer-erigon:dev-pp-6dd537c|g" "$DOCK_CONFIG_FILE"
+
+
+
 make run-new
