01dd43c56bf9a981fee11f834c75c7a75ee5ea76
Niven <sieniven@gmail.com>
2025-06-11 16:02:14 +0800

    Add blocks realtime api

--- turbo/jsonrpc/realtime_api_xlayer.go
+++ turbo/jsonrpc/realtime_api_xlayer.go
@@ -2,17 +2,20 @@ package jsonrpc
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/ledgerwatch/erigon-lib/common"
+	"github.com/ledgerwatch/erigon-lib/common/hexutil"
 	"github.com/ledgerwatch/erigon/core/state"
+	"github.com/ledgerwatch/erigon/rpc"
 	zktypes "github.com/ledgerwatch/erigon/zk/types"
 )
 
 // RealtimeAPI is a collection of functions that are exposed in rpc only
 type RealtimeAPI interface {
 	// // Block related (see ./realtime_blocks_xlayer.go)
-	// BlockNumber(ctx context.Context) (hexutil.Uint64, error)
-	// GetBlockTransactionCountByNumber(ctx context.Context, blockNr rpc.BlockNumber) (*hexutil.Uint, error)
+	BlockNumber(ctx context.Context) (hexutil.Uint64, error)
+	GetBlockTransactionCountByNumber(ctx context.Context, blockNr rpc.BlockNumber) (*hexutil.Uint, error)
 
 	// // Transaction related (see ./realtime_txs_xlayer.go)
 	// GetTransactionByHash(ctx context.Context, hash common.Hash, includeExtraInfo *bool) (interface{}, error)
@@ -50,5 +53,33 @@ func NewRealtimeAPI(
 		ethApi:         base,
 		stateCache:     stateCache,
 		statelessCache: statelessCache,
+	}
+}
+
+func (api *RealtimeAPIImpl) GetBlockNumber(blockNr rpc.BlockNumber) (uint64, bool, error) {
+	currentBlockNumber := api.statelessCache.GetHeight()
+	if currentBlockNumber == 0 {
+		return 0, false, fmt.Errorf("no block number found in stateless cache")
+	}
+
+	switch blockNr {
+	case rpc.LatestBlockNumber:
+		return currentBlockNumber, true, nil
+	case rpc.EarliestBlockNumber:
+		return 0, false, nil
+	case rpc.FinalizedBlockNumber:
+		return currentBlockNumber, true, nil
+	case rpc.SafeBlockNumber:
+		return currentBlockNumber, true, nil
+	case rpc.PendingBlockNumber:
+		return currentBlockNumber, true, nil
+	case rpc.LatestExecutedBlockNumber:
+		return currentBlockNumber, true, nil
+	default:
+		blockNumber := uint64(blockNr.Int64())
+		if blockNumber > currentBlockNumber {
+			return 0, false, fmt.Errorf("block with number %d not found", blockNumber)
+		}
+		return blockNumber, blockNumber == currentBlockNumber, nil
 	}
 }
