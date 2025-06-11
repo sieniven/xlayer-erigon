827e44b8ed427b79980e7d2dc8d05bf35c53cb9f
jianguo <zjg555543@163.com>
2025-06-11 09:51:39 +0800

    update

--- test-fep-pp/5-build-ckd-node.sh
+++ test-fep-pp/5-build-ckd-node.sh
@@ -8,7 +8,7 @@ PWD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 input=${1:-0}
 
 # checking input
-if ! [[ "$input" =~ ^[0-7]$ ]]; then
+if ! [[ "$input" =~ ^[0-9]$ ]]; then
     echo "Error" >&2
     exit 1
 fi
@@ -41,7 +41,7 @@ DOCK_CONFIG_FILE="./docker-compose.yml"
 sed_inplace "s|image: zjg555543/cdk:v0.5.4-rc1|image: cdk|g" "$DOCK_CONFIG_FILE"
 
 CDK_CONFIG_FILE="config/cdk-node-config.toml"
-sed_inplace "s|\(LossCertType[[:space:]]*=[[:space:]]*\)[0-7]|\1$input|g" "$CDK_CONFIG_FILE"
+sed_inplace "s|\(LossCertType[[:space:]]*=[[:space:]]*\)[0-9]|\1$input|g" "$CDK_CONFIG_FILE"
 
 docker-compose stop xlayer-cdk-node
 docker-compose up xlayer-cdk-node
