825ecea6879a7ca1d70bee7bf1bd7b2ed6d36a93
Niven <sieniven@gmail.com>
2025-06-11 18:25:44 +0800

    Add acounts APIs

--- turbo/jsonrpc/realtime_accounts_xlayer.go
+++ turbo/jsonrpc/realtime_accounts_xlayer.go
@@ -1,1 +1,53 @@
 package jsonrpc
+
+import (
+	"context"
+	"fmt"
+	"math/big"
+
+	libcommon "github.com/ledgerwatch/erigon-lib/common"
+	"github.com/ledgerwatch/erigon-lib/common/hexutil"
+	"github.com/ledgerwatch/erigon-lib/common/hexutility"
+	"github.com/ledgerwatch/erigon/common"
+)
+
+func (api *RealtimeAPIImpl) GetBalance(ctx context.Context, address libcommon.Address) (*hexutil.Big, error) {
+	acc, err := api.stateCache.ReadAccountData(address)
+	if err != nil {
+		return nil, fmt.Errorf("cant get a balance for account %x: %w", address.String(), err)
+	}
+	if acc == nil {
+		// Special case - non-existent account is assumed to have zero balance
+		return (*hexutil.Big)(big.NewInt(0)), nil
+	}
+
+	return (*hexutil.Big)(acc.Balance.ToBig()), nil
+}
+
+func (api *RealtimeAPIImpl) GetCode(ctx context.Context, address libcommon.Address) (hexutility.Bytes, error) {
+	acc, err := api.stateCache.ReadAccountData(address)
+	if acc == nil || err != nil {
+		return hexutility.Bytes(""), nil
+	}
+	res, _ := api.stateCache.ReadAccountCode(address, acc.Incarnation, acc.CodeHash)
+	if res == nil {
+		return hexutility.Bytes(""), nil
+	}
+	return res, nil
+}
+
+func (api *RealtimeAPIImpl) GetStorageAt(ctx context.Context, address libcommon.Address, index string) (string, error) {
+	var empty []byte
+
+	acc, err := api.stateCache.ReadAccountData(address)
+	if acc == nil || err != nil {
+		return hexutility.Encode(common.LeftPadBytes(empty, 32)), err
+	}
+
+	location := libcommon.HexToHash(index)
+	res, err := api.stateCache.ReadAccountStorage(address, acc.Incarnation, &location)
+	if err != nil {
+		res = empty
+	}
+	return hexutility.Encode(common.LeftPadBytes(res, 32)), err
+}
--- turbo/jsonrpc/realtime_api_xlayer.go
+++ turbo/jsonrpc/realtime_api_xlayer.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"math/big"
 
-	"github.com/ledgerwatch/erigon-lib/common"
+	libcommon "github.com/ledgerwatch/erigon-lib/common"
 	"github.com/ledgerwatch/erigon-lib/common/hexutil"
 	"github.com/ledgerwatch/erigon-lib/common/hexutility"
 	"github.com/ledgerwatch/erigon/core/state"
@@ -15,7 +15,7 @@ import (
 )
 
 var (
-	mockBlockHash = common.BytesToHash([]byte{1})
+	mockBlockHash = libcommon.BytesToHash([]byte{1})
 )
 
 // RealtimeAPI is a collection of functions that are exposed in rpc only
@@ -25,18 +25,18 @@ type RealtimeAPI interface {
 	GetBlockTransactionCountByNumber(ctx context.Context, blockNr rpc.BlockNumber) (*hexutil.Uint, error)
 
 	// Transaction related (see ./realtime_txs_xlayer.go)
-	GetTransactionByHash(ctx context.Context, hash common.Hash, includeExtraInfo *bool) (interface{}, error)
-	GetRawTransactionByHash(ctx context.Context, hash common.Hash) (hexutility.Bytes, error)
+	GetTransactionByHash(ctx context.Context, hash libcommon.Hash, includeExtraInfo *bool) (interface{}, error)
+	GetRawTransactionByHash(ctx context.Context, hash libcommon.Hash) (hexutility.Bytes, error)
 
 	// Receipt related (see ./realtime_receipts_xlayer.go)
-	GetTransactionReceipt(ctx context.Context, hash common.Hash) (map[string]interface{}, error)
-	GetInternalTransactions(ctx context.Context, hash common.Hash) ([]*zktypes.InnerTx, error)
+	GetTransactionReceipt(ctx context.Context, hash libcommon.Hash) (map[string]interface{}, error)
+	GetInternalTransactions(ctx context.Context, hash libcommon.Hash) ([]*zktypes.InnerTx, error)
 
-	// // Account related (see ./realtime_accounts_xlayer.go)
-	// GetBalance(ctx context.Context, address common.Address, blockNrOrHash rpc.BlockNumberOrHash) (*hexutil.Big, error)
-	// GetTransactionCount(ctx context.Context, address common.Address, blockNrOrHash *rpc.BlockNumberOrHash) (*hexutil.Uint64, error)
-	// GetStorageAt(ctx context.Context, address common.Address, index string, blockNrOrHash rpc.BlockNumberOrHash) (string, error)
-	// GetCode(ctx context.Context, address common.Address, blockNrOrHash rpc.BlockNumberOrHash) (hexutility.Bytes, error)
+	// Account related (see ./realtime_accounts_xlayer.go)
+	GetBalance(ctx context.Context, address libcommon.Address, blockNrOrHash rpc.BlockNumberOrHash) (*hexutil.Big, error)
+	GetTransactionCount(ctx context.Context, address libcommon.Address, blockNrOrHash *rpc.BlockNumberOrHash) (*hexutil.Uint64, error)
+	GetStorageAt(ctx context.Context, address libcommon.Address, index string, blockNrOrHash rpc.BlockNumberOrHash) (string, error)
+	GetCode(ctx context.Context, address libcommon.Address, blockNrOrHash rpc.BlockNumberOrHash) (hexutility.Bytes, error)
 
 	// // Sending related (see ./realtime_call_xlayer.go)
 	// Call(ctx context.Context, args ethapi2.CallArgs, blockNrOrHash rpc.BlockNumberOrHash, overrides *ethapi2.StateOverrides) (hexutility.Bytes, error)
@@ -96,6 +96,6 @@ func (api *RealtimeAPIImpl) getBlockNumber(blockNr rpc.BlockNumber) (uint64, boo
 // Note that realtime API do not support blockHash.
 func newRPCTransaction_realtime(tx types.Transaction, blockNumber uint64, index uint64, baseFee *big.Int) *RPCTransaction {
 	result := NewRPCTransaction(tx, mockBlockHash, blockNumber, index, baseFee)
-	result.BlockHash = &common.Hash{}
+	result.BlockHash = &libcommon.Hash{}
 	return result
 }
