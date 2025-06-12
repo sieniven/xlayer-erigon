6e3f5d6c6f5d7f7fb14ef3c8c42c09079d21aad8
Niven <sieniven@gmail.com>
2025-06-12 15:38:17 +0800

    Fix comments

--- turbo/jsonrpc/realtime_call_xlayer.go
+++ turbo/jsonrpc/realtime_call_xlayer.go
@@ -11,7 +11,7 @@ import (
 	"github.com/ledgerwatch/erigon/turbo/transactions"
 )
 
-// Call implements eth_call.
+// Call implements realtime_call.
 // Executes a new message call immediately without creating a transaction on the block chain.
 // Note that realtime API only supports execution on the latest block.
 func (api *RealtimeAPIImpl) Call(ctx context.Context, args ethapi2.CallArgs, overrides *ethapi2.StateOverrides) (hexutility.Bytes, error) {
