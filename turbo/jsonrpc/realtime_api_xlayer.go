bb364335bf28bc47feef8fc50a160e67b408e9b8
KyrinCode <kyrincode@qq.com>
2025-06-13 18:03:19 +0800

    Revert "revert getTransactionCount"
    This reverts commit 48504d97cae2eadd9cabbc7908c53cd71d686541.

--- turbo/jsonrpc/realtime_api_xlayer.go
+++ turbo/jsonrpc/realtime_api_xlayer.go
@@ -35,6 +35,7 @@ type RealtimeAPI interface {
 
 	// Account related (see ./realtime_accounts_xlayer.go)
 	GetBalance(ctx context.Context, address libcommon.Address) (*hexutil.Big, error)
+	GetTransactionCount(ctx context.Context, address libcommon.Address) (*hexutil.Uint64, error)
 	GetCode(ctx context.Context, address libcommon.Address) (hexutility.Bytes, error)
 	GetStorageAt(ctx context.Context, address libcommon.Address, index string) (string, error)
 
