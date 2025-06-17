c81fd2000c0e41e2931728415c24aae6af0b42ac
cliff.yang <cliff.yang@okg.com>
2025-06-17 18:55:58 +0800

    complete l1 - l2 native bridging

--- test-fep-pp/10-start-remap.sh
+++ test-fep-pp/10-start-remap.sh
@@ -12,6 +12,8 @@ sed_inplace() {
 
 docker stop xlayer-seq && docker rm xlayer-seq
 docker stop xlayer-rpc && docker rm xlayer-rpc
+docker stop xlayer-cdk-node && docker rm xlayer-cdk-node
+docker stop xlayer-agglayer && docker rm xlayer-agglayer
 
 DOCK_CONFIG_FILE="./config/test.erigon.seq.config.yaml"
 #sed_inplace "s|zkevm.executor-strict: false| |g" "$DOCK_CONFIG_FILE"
@@ -24,12 +26,16 @@ sleep 30
 
 docker stop xlayer-seq && docker rm xlayer-seq
 
-rm -rf data/rpc/chaindata && rm -rf data/rpc/smt
+rm -rf data/rpc/chaindata && rm -rf data/rpc/smt && rm -rf data/rpc/caplin && rm -rf data/rpc/snapshots
 cp -rf data/seq/chaindata data/rpc/chaindata
 cp -rf data/seq/smt data/rpc/smt
+cp -rf data/seq/caplin data/rpc/caplin
+cp -rf data/seq/snapshots data/rpc/snapshots
 
-DOCK_CONFIG_FILE="./config/test.erigon.rpc.config.yaml"
-#sed_inplace "s|zkevm.mock-witness-generation: true| |g" "$DOCK_CONFIG_FILE"
+DOCK_CONFIG_FILE="./config/test.erigon.seq.config.yaml"
+sed_inplace "s|zkevm.l2-perform-map: true| zkevm.l2-perform-map: false |g" "$DOCK_CONFIG_FILE"
 
-docker compose -f docker-compose.yml up -d xlayer-seq
+docker compose -f docker-compose.yml up -d xlayer-agglayer
+docker compose -f docker-compose.yml up -d xlayer-cdk-node
 docker compose -f docker-compose.yml up -d xlayer-rpc
+docker compose -f docker-compose.yml up -d xlayer-seq
