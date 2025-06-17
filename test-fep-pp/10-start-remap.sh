67ea801e6e8cc60d406da698aaa7c74cdcab3b13
cliff.yang <cliff.yang@okg.com>
2025-06-17 15:36:53 +0800

    fix deposit

--- test-fep-pp/10-start-remap.sh
+++ test-fep-pp/10-start-remap.sh
@@ -13,18 +13,23 @@ sed_inplace() {
 docker stop xlayer-seq && docker rm xlayer-seq
 docker stop xlayer-rpc && docker rm xlayer-rpc
 
-DOCK_CONFIG_FILE="./docker-compose.yml"
-sed_inplace "s|image: cdk-erigon|image: cdk-erigon-remap|g" "$DOCK_CONFIG_FILE"
-
 DOCK_CONFIG_FILE="./config/test.erigon.seq.config.yaml"
-sed_inplace "s|zkevm.executor-strict: false| |g" "$DOCK_CONFIG_FILE"
-sed_inplace "s|zkevm.witness-full: false| |g" "$DOCK_CONFIG_FILE"
-sed_inplace "s|zkevm.executor-mock: true| |g" "$DOCK_CONFIG_FILE"
-echo "zkevm.l2-perform-map: true" >> "$DOCK_CONFIG_FILE"
+#sed_inplace "s|zkevm.executor-strict: false| |g" "$DOCK_CONFIG_FILE"
+#sed_inplace "s|zkevm.witness-full: false| |g" "$DOCK_CONFIG_FILE"
+#sed_inplace "s|zkevm.executor-mock: true| |g" "$DOCK_CONFIG_FILE"
+echo "\nzkevm.l2-perform-map: true" >> "$DOCK_CONFIG_FILE"
+
+docker compose -f docker-compose.yml up -d xlayer-seq
+sleep 30
+
+docker stop xlayer-seq && docker rm xlayer-seq
+
+rm -rf data/rpc/chaindata && rm -rf data/rpc/smt
+cp -rf data/seq/chaindata data/rpc/chaindata
+cp -rf data/seq/smt data/rpc/smt
 
 DOCK_CONFIG_FILE="./config/test.erigon.rpc.config.yaml"
-sed_inplace "s|zkevm.mock-witness-generation: true| |g" "$DOCK_CONFIG_FILE"
-
+#sed_inplace "s|zkevm.mock-witness-generation: true| |g" "$DOCK_CONFIG_FILE"
 
 docker compose -f docker-compose.yml up -d xlayer-seq
 docker compose -f docker-compose.yml up -d xlayer-rpc
--- test-fep-pp/4-restart-service.sh
+++ test-fep-pp/4-restart-service.sh
@@ -11,12 +11,12 @@ sed_inplace() {
 }
 
 DOCK_CONFIG_FILE="./docker-compose.yml"
-sed_inplace "s|image: cdk-erigon|image: zjg555543/xlayer-erigon:dev-pp-6dd537c|g" "$DOCK_CONFIG_FILE"
+#sed_inplace "s|image: cdk-erigon|image: zjg555543/xlayer-erigon:dev-pp-6dd537c|g" "$DOCK_CONFIG_FILE"
 
 DOCK_CONFIG_FILE="./config/test.erigon.seq.config.yaml"
-sed_inplace "s|zkevm.executor-strict: false| |g" "$DOCK_CONFIG_FILE"
-sed_inplace "s|zkevm.witness-full: false| |g" "$DOCK_CONFIG_FILE"
-sed_inplace "s|zkevm.executor-mock: true| |g" "$DOCK_CONFIG_FILE"
+#sed_inplace "s|zkevm.executor-strict: false| |g" "$DOCK_CONFIG_FILE"
+#sed_inplace "s|zkevm.witness-full: false| |g" "$DOCK_CONFIG_FILE"
+#sed_inplace "s|zkevm.executor-mock: true| |g" "$DOCK_CONFIG_FILE"
 
 DOCK_CONFIG_FILE="./config/test.erigon.rpc.config.yaml"
 sed_inplace "s|zkevm.mock-witness-generation: true| |g" "$DOCK_CONFIG_FILE"
