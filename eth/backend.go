2eff95b5d7c3e6261b8437160c43ead88a3b76f7
Niven <sieniven@gmail.com>
2025-06-20 16:43:41 +0800

    Fix

--- eth/backend.go
+++ eth/backend.go
@@ -1385,7 +1385,7 @@ func New(ctx context.Context, stack *node.Node, config *ethconfig.Config, logger
 				streamClient,
 				dataStreamServer,
 				l1InfoTreeUpdater,
-				backend.realtimeCache.Stateless,
+				backend.realtimeCache,
 				backend.finishChan,
 			)
 
