3a4bc06c4b396703910a3d64559c799ee9dddfeb
Niven <sieniven@gmail.com>
2025-06-20 17:11:50 +0800

    Fix fmt

--- cmd/rpcdaemon/cli/config.go
+++ cmd/rpcdaemon/cli/config.go
@@ -298,7 +298,6 @@ func EmbeddedServices(ctx context.Context,
 
 	txPool = direct.NewTxPoolClient(txPoolServer)
 	mining = direct.NewMiningClient(miningServer)
-
 	ff = rpchelper.New(ctx, eth, txPool, mining, func() {}, logger)
 
 	return
