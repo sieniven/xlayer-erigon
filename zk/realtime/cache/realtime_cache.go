202e3b11ba848a4bd037ce54400117319f287f2a
Niven <sieniven@gmail.com>
2025-07-02 22:40:06 +0800

    Increase pending block queue size threshold

--- zk/realtime/cache/realtime_cache.go
+++ zk/realtime/cache/realtime_cache.go
@@ -25,7 +25,7 @@ const (
 	DefaultStateCacheSize = 1_000_000
 
 	// Sync threshold config
-	PendingBlocksCacheSizeThreshold = 10
+	PendingBlocksCacheSizeThreshold = 20
 )
 
 type PendingBlockContext struct {
