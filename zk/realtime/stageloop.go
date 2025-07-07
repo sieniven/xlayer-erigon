8a1418922259ea285368ecfc58a4bc54ba843852
Niven <sieniven@gmail.com>
2025-07-07 19:47:33 +0800

    Fix handling tx indexes in pending blocks

--- zk/realtime/stageloop.go
+++ zk/realtime/stageloop.go
@@ -2,6 +2,7 @@ package realtime
 
 import (
 	"context"
+	"fmt"
 	"sync/atomic"
 	"time"
 
@@ -47,6 +48,7 @@ func ListenTxKafkaProducer(
 		case blockInfo := <-blockInfoChan:
 			currHeight = blockInfo.Header.Number.Uint64()
 			err = txKafkaProducer.SendKafkaBlockInfo(ctx, blockInfo.Header, blockInfo.TxCount)
+			logger.Debug("[Realtime] Sent block info message", "blockNum", blockInfo.Header.Number, "txCount", blockInfo.TxCount)
 		case txInfo := <-txInfoChan:
 			currHeight = txInfo.BlockNumber
 			if currHeight <= 1 {
@@ -54,6 +56,7 @@ func ListenTxKafkaProducer(
 			}
 			changeset := state.CollectChangeset(txInfo.Entries)
 			err = txKafkaProducer.SendKafkaTransaction(ctx, txInfo.BlockNumber, txInfo.Tx, txInfo.Receipt, txInfo.InnerTxs, changeset)
+			logger.Debug(fmt.Sprintf("[Realtime] Sent tx message for block number %d with txHash %x", txInfo.BlockNumber, txInfo.Tx.Hash()))
 		}
 
 		if err != nil {
