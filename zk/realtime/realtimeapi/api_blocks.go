e2bbaa210df4c54f580d2206bc62c216376e6099
Niven <sieniven@gmail.com>
2025-08-05 12:07:46 +0800

    Refactor

--- zk/realtime/realtimeapi/api_blocks.go
+++ zk/realtime/realtimeapi/api_blocks.go
@@ -59,6 +59,25 @@ func (api *RealtimeAPIImpl) GetBlockTransactionCountByNumber(ctx context.Context
 	return &numOfTx, nil
 }
 
+func (api *RealtimeAPIImpl) GetBlockTransactionCountByHash(ctx context.Context, blockHash libcommon.Hash) (*hexutil.Uint, error) {
+	if api.cacheDB == nil || !api.cacheDB.ReadyFlag.Load() {
+		return api.APIImpl.GetBlockTransactionCountByHash(ctx, blockHash)
+	}
+
+	blockNum, found := api.cacheDB.Stateless.GetBlockNumberByHash(blockHash)
+	if !found {
+		return api.APIImpl.GetBlockTransactionCountByHash(ctx, blockHash)
+	}
+
+	txHashes, ok := api.cacheDB.Stateless.GetBlockTxs(blockNum)
+	if !ok {
+		return api.APIImpl.GetBlockTransactionCountByHash(ctx, blockHash)
+	}
+
+	numOfTx := hexutil.Uint(len(txHashes))
+	return &numOfTx, nil
+}
+
 func (api *RealtimeAPIImpl) GetBlockByNumber(ctx context.Context, blockNr rpc.BlockNumber, fullTx *bool) (map[string]interface{}, error) {
 	if api.cacheDB == nil || !api.cacheDB.ReadyFlag.Load() {
 		return api.APIImpl.GetBlockByNumber(ctx, blockNr, fullTx)
@@ -172,25 +191,6 @@ func (api *RealtimeAPIImpl) GetBlockByHash(ctx context.Context, numberOrHash rpc
 	}
 
 	return api.APIImpl.GetBlockByHash(ctx, numberOrHash, fullTx)
-}
-
-func (api *RealtimeAPIImpl) GetBlockTransactionCountByHash(ctx context.Context, blockHash libcommon.Hash) (*hexutil.Uint, error) {
-	if api.cacheDB == nil || !api.cacheDB.ReadyFlag.Load() {
-		return api.APIImpl.GetBlockTransactionCountByHash(ctx, blockHash)
-	}
-
-	blockNum, found := api.cacheDB.Stateless.GetBlockNumberByHash(blockHash)
-	if !found {
-		return api.APIImpl.GetBlockTransactionCountByHash(ctx, blockHash)
-	}
-
-	txHashes, ok := api.cacheDB.Stateless.GetBlockTxs(blockNum)
-	if !ok {
-		return api.APIImpl.GetBlockTransactionCountByHash(ctx, blockHash)
-	}
-
-	numOfTx := hexutil.Uint(len(txHashes))
-	return &numOfTx, nil
 }
 
 func (api *RealtimeAPIImpl) GetBlockInternalTransactions(ctx context.Context, blockNr rpc.BlockNumber) (map[libcommon.Hash][]*zktypes.InnerTx, error) {
