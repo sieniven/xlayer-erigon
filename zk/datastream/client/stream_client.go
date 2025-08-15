c67985086d5340d6589fe88456d95f8d8e4bc06e
KyrinCode <kyrincode@qq.com>
2025-08-15 21:59:10 +0800

    update

--- zk/datastream/client/stream_client.go
+++ zk/datastream/client/stream_client.go
@@ -328,7 +328,7 @@ func (c *StreamClient) Stop() error {
 // Returns the current status of the header.
 // If started, terminate the connection.
 func (c *StreamClient) GetHeader() (*types.HeaderEntry, error) {
-	log.Debug("[Datastream client] Getting header", "client", c.conn)
+	log.Info("[Datastream client] Getting header", "client", c.conn)
 	if err := c.stopStreaming(); err != nil {
 		return nil, fmt.Errorf("stopStreaming: %w", err)
 	}
@@ -360,14 +360,14 @@ func (c *StreamClient) GetHeader() (*types.HeaderEntry, error) {
 	}
 
 	c.header = h
-	log.Debug("[Datastream client] getHeader", "header", c.header)
+	log.Info("[Datastream client] getHeader", "header", c.header)
 
 	return h, nil
 }
 
 // sendEntryCmdWrapper sends CmdEntry command and reads packet type and decodes result entry.
 func (c *StreamClient) sendEntryCmdWrapper(entryNum uint64) error {
-	log.Debug("[Datastream client] Sending entry command", "entryNum", entryNum)
+	log.Info("[Datastream client] Sending entry command", "entryNum", entryNum)
 	if err := c.sendEntryCmd(entryNum); err != nil {
 		return fmt.Errorf("sendEntryCmd: %w", err)
 	}
