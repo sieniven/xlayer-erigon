336ee0b874097d7bd1016a1a2841581a6f2ac270
haogeng xie <903932861@qq.com>
2025-08-18 11:54:23 +0800

    Update

--- eth/backend_xlayer.go
+++ eth/backend_xlayer.go
@@ -56,6 +56,15 @@ func (s *Ethereum) listenApollo(ctx context.Context, cfg *ethconfig.Config) {
 			if slices.Contains(ethCfg.XLayer.ApolloChanged, utils.DynamicBlockGasLimit.Name) {
 				cfg.Zk.XLayer.DynamicBlockGasLimit = ethCfg.XLayer.DynamicBlockGasLimit
 			}
+			if slices.Contains(ethCfg.XLayer.ApolloChanged, utils.BridgeInterceptWhitelistEnabled.Name) {
+				cfg.Zk.XLayer.BridgeIntercept.WhitelistEnabled = ethCfg.XLayer.BridgeIntercept.WhitelistEnabled
+			}
+			if slices.Contains(ethCfg.XLayer.ApolloChanged, utils.BridgeInterceptMaxBridgeAmount.Name) {
+				cfg.Zk.XLayer.BridgeIntercept.MaxBridgeAmount = ethCfg.XLayer.BridgeIntercept.MaxBridgeAmount
+			}
+			if slices.Contains(ethCfg.XLayer.ApolloChanged, utils.BridgeInterceptWhitelistAddresses.Name) {
+				cfg.Zk.XLayer.BridgeIntercept.WhitelistAddresses = ethCfg.XLayer.BridgeIntercept.WhitelistAddresses
+			}
 			if l1SyncerConfigChanged {
 				s.updateAllL1Syncer(cfg.Zk.XLayer.GetLogsTimeout, cfg.Zk.XLayer.GetLogsRetries)
 			}
